588b693bfe47bdadc5fbeb10e1474f6062be14eb
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-06-08 18:34:50 +0100

    created a div to store the value of the errors

--- Challenge_1/main.js
+++ Challenge_1/main.js
@@ -1,23 +1,43 @@
-// adding an email validation function 
-function validateEmail(email) {
-    const re = /\S+@\S+\.\S+/;
-    return re.test(email);
+
+
+// const form = document.querySelector('form');
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     const email = emailInput.value;
+
+//     if (!email) {
+//         alert('Enter something before continung na');
+//     }
+//     else if(!validateEmail(email)){
+    //         alert('Enter a valid email before clicking the submit button');
+//     }
+//     else{
+    //         alert('thanks for submitting the email');
+    //     }
+    // });
+    
+    function submitForm() {
+        // getting the value from the email input
+        const email = document.getElementById('email').value;
+
+        // validates the email
+        if(!validateEmail(email)){
+            alert('Invalid Email address');
+            return;
+        }
+        window.location.href = "success.html";
+
+        // adding an email validation function 
+    function validateEmail(email) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+        }
 }
 
-const form = document.querySelector('form');
-const emailInput = document.querySelector('#email');
-
-form.addEventListener('submit', (event) => {
+const submitForm = document.getElementById('form');
+form.addEventListener('submit', function (event) {
+    // this prevents the default form from being submitted    
     event.preventDefault();
-    const email = emailInput.value;
-
-    if (!email) {
-        alert('Enter something before continung na');
-    }
-    else if(!validateEmail(email)){
-        alert('Enter a valid email before clicking the submit button');
-    }
-    else{
-        alert('thanks for submitting the email');
-    }
+    submitForm();
 });
