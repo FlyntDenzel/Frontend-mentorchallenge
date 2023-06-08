41be2aeee104f502e3126b2f9b81a6a79ef8795a
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-06-08 10:35:17 +0100

    adding event listener to the button

--- Challenge_1/main.js
+++ Challenge_1/main.js
@@ -3,3 +3,10 @@ function validateEmail(email) {
     const re = /\S+@\S+\.\S+/;
     return re.test(email);
 }
+
+const form = document.querySelector('form');
+const emailInput = document.querySelector('#email');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault
+})
