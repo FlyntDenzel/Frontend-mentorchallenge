77085f1db90135084c113fb3dd1d8826866bb4c2
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-06-28 21:23:07 +0100

    created various components

---
+++ challenge6/src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./header";
+import Content from "./content";
+
+function App() {
+    return <div>
+        <Header />
+        <Content /> 
+    </div>
+}
+
+export default App;
