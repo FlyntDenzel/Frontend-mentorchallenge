960af72b6211d5773fe8e5011187b194f99f0de9
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-06-30 08:18:28 +0100

    linked all jsx files

---
+++ challenge7/src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header/header';
+import Content from './components/content/content';
+import Footer from './components/footer/footer';
+
+function App(){
+    return<div>
+        <Header />
+        <Content />
+        <Footer />
+    </div>
+}
+
+export default App;
---
+++ challenge7/src/components/content/content.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Content(){
+    return<div>
+        hello world
+    </div>
+}
+
+export default Content;
